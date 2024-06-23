cb1c63997b3167ea8a987a9213bcd1036029216c
Nxt Gabriel <161211651+dxzenith@users.noreply.github.com>
2024-06-23 12:25:25 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
 
+echo "####################################################"
+echo "#                                                  #"
+echo "#         Welcome to Zun Script                    #"
+echo "#                                                  #"
+echo "####################################################"
+
+sudo apt update -y
+sudo apt install screen -y
+screen -S farcaster
+curl -sSL https://download.thehubble.xyz/bootstrap.sh | bash

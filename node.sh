3370cbee1c9024db5b848e07f2ee2deba64ffa7d
Nxt Gabriel <161211651+dxzenith@users.noreply.github.com>
2024-06-23 13:17:42 +0530

    Update node.sh

--- node.sh
+++ node.sh
@@ -9,4 +9,3 @@ echo "####################################################"
 sudo apt update -y
 sudo apt install screen -y
 screen -S farcaster
-screen -dmS farcaster bash -c "curl -sSL https://download.thehubble.xyz/bootstrap.sh | bash"
